49e6343f1594d1e71fd0032a23437760151f7349
Tabor Kelly <taborkelly+github@gmail.com>
2015-09-14 06:08:49 -0700

    Rearranging mod.rs.
    Signed-off-by: Tabor Kelly <taborkelly+github@gmail.com>

--- src/nl/mod.rs
+++ src/nl/mod.rs
@@ -14,37 +14,6 @@ use ::std::fmt;
 
 use ::num::FromPrimitive;
 
-#[derive(Debug)]
-#[derive(Default)]
-pub struct Nlmsghdr {
-    pub nlmsg_len: u32,
-    pub nlmsg_type: u16,
-    pub nlmsg_flags: u16,
-    pub nlmsg_seq: u32,
-    pub nlmsg_pid: u32,
-}
-impl fmt::Display for Nlmsghdr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{\n\tnlmsg_len: {},\n\tnlmsg_type: {},\n\t\
-               nlmsg_flags: {:#x},\n\tnlmsg_seq: {},\n\tnlmsg_pid: {}\n}}",
-               self.nlmsg_len, self.nlmsg_type, self.nlmsg_flags,
-               self.nlmsg_seq, self.nlmsg_pid)
-    }
-}
-
-// Netlink header is native endian
-pub fn read_header(data: &[u8]) -> Nlmsghdr {
-    let mut s = Nlmsghdr::default();
-    let mut cursor = Cursor::new(data);
-
-    s.nlmsg_len = cursor.read_u32::<NativeEndian>().unwrap();
-    s.nlmsg_type = cursor.read_u16::<NativeEndian>().unwrap();
-    s.nlmsg_flags = cursor.read_u16::<NativeEndian>().unwrap();
-    s.nlmsg_seq = cursor.read_u32::<NativeEndian>().unwrap();
-    s.nlmsg_pid = cursor.read_u32::<NativeEndian>().unwrap();
-    s
-}
-
 // Cooked SLL header is big endian (network byte order)
 // http://www.tcpdump.org/linktypes/LINKTYPE_LINUX_SLL.html
 #[derive(Debug)]
@@ -100,19 +69,35 @@ pub fn read_cooked_header(data: &[u8]) -> CookedHeader {
     c
 }
 
-#[test]
-fn test_read_header() {
-    let raw_data = [0u8, 4, 3, 56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 36, 0,
-                    0, 0, 26, 0, 5, 3, 89, 7, 185, 85, 249, 2, 128, 0, 32, 0,
-                    0, 0, 8, 0, 3, 0, 2, 0, 0, 0, 8, 0, 1, 0, 0, 0, 0, 0];
-    let h = read_header(&raw_data[COOKED_HEADER_SIZE ..]);
-    println!("h = {:?}", h);
+#[derive(Debug)]
+#[derive(Default)]
+pub struct Nlmsghdr {
+    pub nlmsg_len: u32,
+    pub nlmsg_type: u16,
+    pub nlmsg_flags: u16,
+    pub nlmsg_seq: u32,
+    pub nlmsg_pid: u32,
+}
+impl fmt::Display for Nlmsghdr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{\n\tnlmsg_len: {},\n\tnlmsg_type: {},\n\t\
+               nlmsg_flags: {:#x},\n\tnlmsg_seq: {},\n\tnlmsg_pid: {}\n}}",
+               self.nlmsg_len, self.nlmsg_type, self.nlmsg_flags,
+               self.nlmsg_seq, self.nlmsg_pid)
+    }
+}
 
-    assert!(h.nlmsg_len == 36);
-    assert!(h.nlmsg_type == 26);
-    assert!(h.nlmsg_flags == 773);
-    assert!(h.nlmsg_seq == 1438189401);
-    assert!(h.nlmsg_pid == 8389369);
+// Netlink header is native endian
+pub fn read_header(data: &[u8]) -> Nlmsghdr {
+    let mut s = Nlmsghdr::default();
+    let mut cursor = Cursor::new(data);
+
+    s.nlmsg_len = cursor.read_u32::<NativeEndian>().unwrap();
+    s.nlmsg_type = cursor.read_u16::<NativeEndian>().unwrap();
+    s.nlmsg_flags = cursor.read_u16::<NativeEndian>().unwrap();
+    s.nlmsg_seq = cursor.read_u32::<NativeEndian>().unwrap();
+    s.nlmsg_pid = cursor.read_u32::<NativeEndian>().unwrap();
+    s
 }
 
 #[test]
@@ -130,4 +115,19 @@ fn test_read_cooked_header() {
         assert!(*a == 0);
     }
     assert!(h.netlink_family == netlink::NetlinkFamily::NETLINK_GENERIC);
+}
+
+#[test]
+fn test_read_header() {
+    let raw_data = [0u8, 4, 3, 56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 36, 0,
+                    0, 0, 26, 0, 5, 3, 89, 7, 185, 85, 249, 2, 128, 0, 32, 0,
+                    0, 0, 8, 0, 3, 0, 2, 0, 0, 0, 8, 0, 1, 0, 0, 0, 0, 0];
+    let h = read_header(&raw_data[COOKED_HEADER_SIZE ..]);
+    println!("h = {:?}", h);
+
+    assert!(h.nlmsg_len == 36);
+    assert!(h.nlmsg_type == 26);
+    assert!(h.nlmsg_flags == 773);
+    assert!(h.nlmsg_seq == 1438189401);
+    assert!(h.nlmsg_pid == 8389369);
 }
